a67c7e5ecc24181a84fa6548ba4db7fdb06bbc89
Wendy-Okoth <wendyaokoth@gmail.com>
2025-11-29 19:53:16 +0300

    feat: add gallery section, footer icons, and updated styles for Big Five project

--- projectBig5/firstApp/urls.py
+++ projectBig5/firstApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('search/', views.search, name='search'),
 ]
--- projectBig5/firstApp/views.py
+++ projectBig5/firstApp/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def homePage(request):
     return render(request,"main.html")
+
+def search(request):
+    query = request.GET.get('q', '').lower()
+    animals = {
+        "lion": "images/lion.jpg",
+        "leopard": "images/leopard.jpg",
+        "elephant": "images/elephant.jpg",
+        "rhino": "images/rhino.jpg",
+        "buffalo": "images/buffalo.jpg",
+    }
+    result = animals.get(query)
+    return render(request, "search.html", {"query": query, "result": result})
